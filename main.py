9870ebdee8449e1e7b59adde42fc715a8d47492e
unknown <priyabiswas12@gmail.com>
2024-10-19 17:14:40 +0200

    data validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.wine_quality_classification import logger
 from wine_quality_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from wine_quality_classificationpipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from wine_quality_classification.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from wine_quality_classification.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from wine_quality_classification.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from wine_quality_classification.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -15,3 +15,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
