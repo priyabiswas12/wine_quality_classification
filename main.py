d2e49d5bc39a8380910f002412df2908d32dadbb
unknown <priyabiswas12@gmail.com>
2024-10-22 13:23:37 +0200

    model trainer added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.wine_quality_classification import logger
 from wine_quality_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from wine_quality_classification.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from wine_quality_classification.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-#from wine_quality_classification.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from wine_quality_classification.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from wine_quality_classification.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
@@ -41,3 +41,14 @@ except Exception as e:
         raise e
 
 
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
